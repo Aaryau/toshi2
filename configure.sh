b3a5e0b164782bbda9ee2097628516adb4a3d1d9
Aaryau <108529599+Aaryau@users.noreply.github.com>
2022-07-01 18:24:58 +0530

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,33 @@
+#configure.sh Aaryan_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+
+#disable spotlight indexing
+sudo mdutil -i off -a
+
+#Create new account
+sudo dscl . -create /Users/Aaryan
+sudo dscl . -create /Users/Aaryan UserShell /bin/bash
+sudo dscl . -create /Users/Aaryan RealName "Aaryan"
+sudo dscl . -create /Users/Aaryan UniqueID 1001
+sudo dscl . -create /Users/Aaryan PrimaryGroupID 80
+sudo dscl . -create /Users/Aaryan NFSHomeDirectory /Users/Aaryan
+sudo dscl . -passwd /Users/Aaryan $1
+sudo dscl . -passwd /Users/Aaryan $1
+sudo createhomedir -c -u Aaryan > /dev/null
+
+#Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+
+#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+#Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+
+#install ngrok
+brew install ngrok --cask
+
+#configure ngrok and start it
+ngrok authtoken $3
+ngrok tcp 5900 &
